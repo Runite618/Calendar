6048a4b1b2ac6981b3b545b534dbf40141cd8b38
Matthew Mo <matthew_mo@hotmail.co.uk>
2017-01-21 16:07:47 +0000

    Added in new stuff, calendar control added with date webpage.

--- Calendar/Controllers/HomeController.cs
+++ Calendar/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Calendar.Models;
 
 namespace Calendar.Controllers
 {
@@ -11,6 +12,12 @@ namespace Calendar.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Date()
+        {
+            var model = new Date();
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Calendar/Models/Date.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Calendar.Models
+{
+    public class Date
+    {
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
